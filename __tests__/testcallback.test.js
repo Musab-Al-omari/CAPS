1b7c4e3d2a2af3c1a491888bb37a36936c8e5914
Musab-Al-omari <musabsomari@gmail.com>
2021-05-23 19:33:48 +0300

    test1

--- __tests__/testcallback.test.js
+++ __tests__/testcallback.test.js
@@ -9,7 +9,7 @@ describe('unit tests for each event handler function ', () => {
   jest.spyOn(global.console, 'log');
   it('main console log in my js', () => {
     let obj = {
-      storeName: process.env.storeName || musabStore,
+      storeName: process.env.storeName || 'musabStore',
       orderId: faker.datatype.uuid(),
       customerName: faker.name.findName(),
       address: faker.address.city()
